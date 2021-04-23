ecb87d503a58dee9c43f315d95272848ab721513
chentong2012 <1952613124@qq.com>
2021-04-23 22:40:42 +0800

    删除图书功能

---
+++ Delete.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Delete{
+    public void delete(int id) {
+        Connection connection = DBconnection.getConnection();
+        PreparedStatement statement1 = null;
+        try {
+            String deleteStr = "delete from readbook where id=?";
+            statement1 = connection.prepareStatement(deleteStr);
+            statement1.setInt(1, id);
+            int i = statement1.executeUpdate();
+            if (i >= 0) {
+                System.out.println("删除成功");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement1 != null) {
+                try {
+                    statement1.close();  //关闭连接
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close(); //关闭连接
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
